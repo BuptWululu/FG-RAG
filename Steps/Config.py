05ecfbb69709b9f53de0d54653a77192f4ebebae
BuptWululu <2971025505@qq.com>
2025-01-23 09:57:16 +0800

    modify

--- Steps/Config.py
+++ Steps/Config.py
@@ -3,7 +3,7 @@ from datetime import datetime
 log_name = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '.txt'
 work_dir = 'Datasets/example'
 query_file = 'example.json'
-query_type = 'default'
+query_type = 'default' # Switch it to 'precise' to get an answer in the form of a word or entity.
 
 chunk_size = 1200
 chunk_overlap = 100
@@ -12,23 +12,23 @@ max_gleaning_times = 1
 generator_api_key = 'sk-xxx'
 generator_base_url = 'https:'
 generator_name = 'gpt-4o-mini'
-generate_parallel = 32
+generate_parallel = 32 # the number of parallel requests that may be made
 generate_request_timeout = 60
 generate_max_retries = 20
 
 encoder_api_key = 'sk-xxx'
 encoder_base_url = 'https://'
 encoder_name = 'text-embedding-3-small'
-encode_batch_size = 32
-encode_parallel = 32
+encode_batch_size = 32 # the number of documents to send in a single request
+encode_parallel = 32 # the number of parallel requests that may be made
 encode_request_timeout = 120
 encode_max_retries = 20
 
 initial_selected_entity_number = 20
 max_description_number = 75
-max_extract_entity_number = 9
+max_extract_entity_number = 9 
 
-naive_topk = 10
+naive_topk = 10 # the number of text chunks retrieved in NaiveRAG
 
 tuple_delimiter = '<|>'
 record_delimiter = '##'
